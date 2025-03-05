d7256dad59be2eac3b19b0003941ea468e08f0c8
kitkatbut <morslichafika0@gmail.com>
2025-03-05 19:58:57 +0100

    Ajout menu & titre h1 Mise en place de la structure HTML pour la nav Mise en place du css pour la nv et le h1

--- index.php
+++ index.php
@@ -4,9 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>App Diary</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
     <link rel="stylesheet" href='css/style.css'>
 </head>
 <body>
+
+   <header>
+    <nav>
+        <ul>
+            <li>
+                <a href="./index.php">
+                    <img src="./images/svg/logo.svg" alt="Logo My Journal">
+                My Journal</li>
+                </a>
+                
+            <li><a class="nav__item new__entry__item"><img src="./images/svg/plus.svg" alt="icon plus">
+                New Entry</button></a></li>
+        </ul>
+    </nav>
+  </header>
+
     <h1>App Diary</h1>
 </body>
 </html>
