444deb6489ce113858005ab0fe2b4a11e5c6742e
kitkatbut <morslichafika0@gmail.com>
2025-03-05 20:27:33 +0100

    Modification méthode insertion logo Méthode d'intégration directe du code SVG au lieu de  Méthode classique avec la balise <img>

--- index.php
+++ index.php
@@ -14,11 +14,15 @@
    <header>
     <nav>
         <ul>
-            <li>
-                <a href="./index.php">
-                    <img src="./images/svg/logo.svg" alt="Logo My Journal">
-                My Journal</li>
-                </a>
+        <li>
+            <a href="./index.php" class="nav__item nav__brand">
+                <svg viewBox="0 0 60.7863869853 60.7863869853" aria-hidden="true" class="nav__brand-image">
+                    <path class="svg__brand" d="m45.589790239,30.3931934927c8.3928407749,0,15.1965967463-6.8037559715,15.1965967463-15.1965967463S53.9826310139,0,45.589790239,0H15.196554313C6.8037135382,0,0,6.8037559715,0,15.1965967463v30.3931934927c0,8.3928407749,6.8037135382,15.1965967463,15.196554313,15.1965967463h30.393235926c8.3928407749,0,15.1965967463-6.8037559715,15.1965967463-15.1965967463s-6.8037559715-15.1965967463-15.1965967463-15.1965967463Z"/>
+                </svg>
+                My Journal
+            </a>
+        </li>
+                
                 
             <li><a class="nav__item new__entry__item"><img src="./images/svg/plus.svg" alt="icon plus">
                 New Entry</a></li>
