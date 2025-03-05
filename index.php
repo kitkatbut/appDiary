110288bda41774f6212a44f3f8cdeec4d9094b3e
kitkatbut <morslichafika0@gmail.com>
2025-03-06 00:32:37 +0100

    Ajout de la structure card Ajout des images Ajout du design des cards

--- index.php
+++ index.php
@@ -11,35 +11,48 @@
 </head>
 <body>
 
-   <header>
-    <nav>
-        <ul>
-        <li>
-            <a href="./index.php" class="nav__item nav__brand">
-                <svg viewBox="0 0 60.7863869853 60.7863869853" aria-hidden="true" class="nav__brand-image">
-                    <path class="svg__brand" d="m45.589790239,30.3931934927c8.3928407749,0,15.1965967463-6.8037559715,15.1965967463-15.1965967463S53.9826310139,0,45.589790239,0H15.196554313C6.8037135382,0,0,6.8037559715,0,15.1965967463v30.3931934927c0,8.3928407749,6.8037135382,15.1965967463,15.196554313,15.1965967463h30.393235926c8.3928407749,0,15.1965967463-6.8037559715,15.1965967463-15.1965967463s-6.8037559715-15.1965967463-15.1965967463-15.1965967463Z"/>
-                </svg>
-                My Journal
-            </a>
-        </li>
-                
-                
+    <header>
+        <nav>
+            <ul>
             <li>
-                <a href="form.php" class="nav__item new__entry__item" aria-hidden="true">
-                <!--<img src="./images/svg/plus.svg" alt="icon plus">-->
-                <svg width="44.4901230052" height="44.4901230053" viewBox="0 0 44.4901230052 44.4901230053"class="nav__plus__image">
-                   <g class="svg__plus">
-                        <circle  cx="22.2450615026" cy="22.2450615026" r="21.2450615026"/>
-                        <line  x1="22.2450615026" y1="13.4699274037" x2="22.2450615026" y2="31.0201956015"/>
-                        <line  x1="31.0201956015" y1="22.2450658041" x2="13.4699274037" y2="22.2450572011"/>
-                    </g>
-                </svg>
-                New Entry</a>
+                <a href="./index.php" class="nav__item nav__brand">
+                    <svg viewBox="0 0 60.7863869853 60.7863869853" aria-hidden="true" class="nav__brand-image">
+                        <path class="svg__brand" d="m45.589790239,30.3931934927c8.3928407749,0,15.1965967463-6.8037559715,15.1965967463-15.1965967463S53.9826310139,0,45.589790239,0H15.196554313C6.8037135382,0,0,6.8037559715,0,15.1965967463v30.3931934927c0,8.3928407749,6.8037135382,15.1965967463,15.196554313,15.1965967463h30.393235926c8.3928407749,0,15.1965967463-6.8037559715,15.1965967463-15.1965967463s-6.8037559715-15.1965967463-15.1965967463-15.1965967463Z"/>
+                    </svg>
+                    My Journal
+                </a>
             </li>
-        </ul>
-    </nav>
-  </header>
+                    
+                    
+                <li>
+                    <a href="form.php" class="nav__item new__entry__item" aria-hidden="true">
+                    <!--<img src="./images/svg/plus.svg" alt="icon plus">-->
+                    <svg width="30" height="30" viewBox="0 0 44.4901230052 44.4901230053"class="nav__plus__image">
+                    <g class="svg__plus">
+                            <circle  cx="22.2450615026" cy="22.2450615026" r="21.2450615026"/>
+                            <line  x1="22.2450615026" y1="13.4699274037" x2="22.2450615026" y2="31.0201956015"/>
+                            <line  x1="31.0201956015" y1="22.2450658041" x2="13.4699274037" y2="22.2450572011"/>
+                        </g>
+                    </svg>
+                    New Entry</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    
+    <main>
+        <h1>App Diary</h1>
 
-    <h1>App Diary</h1>
+        <div class="card">
+            <img src="./images/pexels-kaushal-moradiya-2781195.jpg" alt="Homme lisant livre intitulé space encyclopedia" class="card__right card__img">
+            <div class="card__left">
+                <p class="card__left__subtitle">WEEK 1</p>
+                <h2 class="card__left__title">Lorem impsum</h2>
+                <hr class="card__left__trait">
+                <p class="card__left__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolores consectetur distinctio blanditiis a asperiores. Voluptatem distinctio, suscipit molestias, dignissimos accusamus facere, debitis consequuntur cumque deserunt qui nemo maiores unde?</p>
+            </div>
+        </div>
+    </main>
+
 </body>
 </html>
