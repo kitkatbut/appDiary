27c61db68321985fa9da1780de52c2f4be2ac087
kitkatbut <morslichafika0@gmail.com>
2025-03-06 10:21:45 +0100

    Responsive design Ajout des mediaqueries Passage mobile-first

--- index.php
+++ index.php
@@ -44,6 +44,24 @@
         <h1>App Diary</h1>
 
         <div class="card">
+            <img src="./images/pexels-tranmautritam-68761.jpg" alt="Homme lisant livre intitulé space encyclopedia" class="card__right card__img">
+            <div class="card__left">
+                <p class="card__left__subtitle">WEEK 1</p>
+                <h2 class="card__left__title">Lorem impsum</h2>
+                <hr class="card__left__trait">
+                <p class="card__left__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolores consectetur distinctio blanditiis a asperiores. Voluptatem distinctio, suscipit molestias, dignissimos accusamus facere, debitis consequuntur cumque deserunt qui nemo maiores unde?</p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="./images/pexels-lumn-167682.jpg" alt="Homme lisant livre intitulé space encyclopedia" class="card__right card__img">
+            <div class="card__left">
+                <p class="card__left__subtitle">WEEK 1</p>
+                <h2 class="card__left__title">Lorem impsum</h2>
+                <hr class="card__left__trait">
+                <p class="card__left__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolores consectetur distinctio blanditiis a asperiores. Voluptatem distinctio, suscipit molestias, dignissimos accusamus facere, debitis consequuntur cumque deserunt qui nemo maiores unde?</p>
+            </div>
+        </div>
+        <div class="card">
             <img src="./images/pexels-kaushal-moradiya-2781195.jpg" alt="Homme lisant livre intitulé space encyclopedia" class="card__right card__img">
             <div class="card__left">
                 <p class="card__left__subtitle">WEEK 1</p>
@@ -52,6 +70,17 @@
                 <p class="card__left__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolores consectetur distinctio blanditiis a asperiores. Voluptatem distinctio, suscipit molestias, dignissimos accusamus facere, debitis consequuntur cumque deserunt qui nemo maiores unde?</p>
             </div>
         </div>
+        <div class="card">
+            <img src="./images/pexels-canva-studio-3153199.jpg" alt="Homme lisant livre intitulé space encyclopedia" class="card__right card__img">
+            <div class="card__left">
+                <p class="card__left__subtitle">WEEK 1</p>
+                <h2 class="card__left__title">Lorem impsum</h2>
+                <hr class="card__left__trait">
+                <p class="card__left__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolores consectetur distinctio blanditiis a asperiores. Voluptatem distinctio, suscipit molestias, dignissimos accusamus facere, debitis consequuntur cumque deserunt qui nemo maiores unde?</p>
+            </div>
+        </div>
+
+
     </main>
 
 </body>
