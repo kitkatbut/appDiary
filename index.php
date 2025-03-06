73f51bfe468982fa4124f41f7364e0cef6476309
kitkatbut <morslichafika0@gmail.com>
2025-03-06 10:48:14 +0100

    Ajout structure et desigin pagination

--- index.php
+++ index.php
@@ -80,7 +80,16 @@
             </div>
         </div>
 
-
+        <!-- pagination -->
+         <ul class="card__pagination">
+            <li><a href="#">&lt;</a></li>
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+            <li><a href="#">&gt;</a></li>
+         </ul>
     </main>
 
 </body>
