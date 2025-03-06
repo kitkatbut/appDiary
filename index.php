0f82e5adb1c27f0701c1f3aca3dcc753696cd563
kitkatbut <morslichafika0@gmail.com>
2025-03-06 17:43:40 +0100

    Ajout du background

--- index.php
+++ index.php
@@ -40,56 +40,58 @@
         </nav>
     </header>
     
-    <main>
+    <main class="main">
         <h1>App Diary</h1>
+        <section class="card__container">
 
-        <div class="card">
-            <img src="./images/pexels-tranmautritam-68761.jpg" alt="Homme lisant livre intitulé space encyclopedia" class="card__right card__img">
-            <div class="card__left">
-                <p class="card__left__subtitle">WEEK 1</p>
-                <h2 class="card__left__title">Lorem impsum</h2>
-                <hr class="card__left__trait">
-                <p class="card__left__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolores consectetur distinctio blanditiis a asperiores. Voluptatem distinctio, suscipit molestias, dignissimos accusamus facere, debitis consequuntur cumque deserunt qui nemo maiores unde?</p>
+            <div class="card">
+                <img src="./images/pexels-tranmautritam-68761.jpg" alt="Homme lisant livre intitulé space encyclopedia" class="card__right card__img">
+                <div class="card__left">
+                    <p class="card__left__subtitle">WEEK 1</p>
+                    <h2 class="card__left__title">Lorem impsum</h2>
+                    <hr class="card__left__trait">
+                    <p class="card__left__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolores consectetur distinctio blanditiis a asperiores. Voluptatem distinctio, suscipit molestias, dignissimos accusamus facere, debitis consequuntur cumque deserunt qui nemo maiores unde?</p>
+                </div>
             </div>
-        </div>
-        <div class="card">
-            <img src="./images/pexels-lumn-167682.jpg" alt="Homme lisant livre intitulé space encyclopedia" class="card__right card__img">
-            <div class="card__left">
-                <p class="card__left__subtitle">WEEK 1</p>
-                <h2 class="card__left__title">Lorem impsum</h2>
-                <hr class="card__left__trait">
-                <p class="card__left__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolores consectetur distinctio blanditiis a asperiores. Voluptatem distinctio, suscipit molestias, dignissimos accusamus facere, debitis consequuntur cumque deserunt qui nemo maiores unde?</p>
+            <div class="card">
+                <img src="./images/pexels-lumn-167682.jpg" alt="Homme lisant livre intitulé space encyclopedia" class="card__right card__img">
+                <div class="card__left">
+                    <p class="card__left__subtitle">WEEK 1</p>
+                    <h2 class="card__left__title">Lorem impsum</h2>
+                    <hr class="card__left__trait">
+                    <p class="card__left__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolores consectetur distinctio blanditiis a asperiores. Voluptatem distinctio, suscipit molestias, dignissimos accusamus facere, debitis consequuntur cumque deserunt qui nemo maiores unde?</p>
+                </div>
             </div>
-        </div>
-        <div class="card">
-            <img src="./images/pexels-kaushal-moradiya-2781195.jpg" alt="Homme lisant livre intitulé space encyclopedia" class="card__right card__img">
-            <div class="card__left">
-                <p class="card__left__subtitle">WEEK 1</p>
-                <h2 class="card__left__title">Lorem impsum</h2>
-                <hr class="card__left__trait">
-                <p class="card__left__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolores consectetur distinctio blanditiis a asperiores. Voluptatem distinctio, suscipit molestias, dignissimos accusamus facere, debitis consequuntur cumque deserunt qui nemo maiores unde?</p>
+            <div class="card">
+                <img src="./images/pexels-kaushal-moradiya-2781195.jpg" alt="Homme lisant livre intitulé space encyclopedia" class="card__right card__img">
+                <div class="card__left">
+                    <p class="card__left__subtitle">WEEK 1</p>
+                    <h2 class="card__left__title">Lorem impsum</h2>
+                    <hr class="card__left__trait">
+                    <p class="card__left__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolores consectetur distinctio blanditiis a asperiores. Voluptatem distinctio, suscipit molestias, dignissimos accusamus facere, debitis consequuntur cumque deserunt qui nemo maiores unde?</p>
+                </div>
             </div>
-        </div>
-        <div class="card">
-            <img src="./images/pexels-canva-studio-3153199.jpg" alt="Homme lisant livre intitulé space encyclopedia" class="card__right card__img">
-            <div class="card__left">
-                <p class="card__left__subtitle">WEEK 1</p>
-                <h2 class="card__left__title">Lorem impsum</h2>
-                <hr class="card__left__trait">
-                <p class="card__left__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolores consectetur distinctio blanditiis a asperiores. Voluptatem distinctio, suscipit molestias, dignissimos accusamus facere, debitis consequuntur cumque deserunt qui nemo maiores unde?</p>
+            <div class="card">
+                <img src="./images/pexels-canva-studio-3153199.jpg" alt="Homme lisant livre intitulé space encyclopedia" class="card__right card__img">
+                <div class="card__left">
+                    <p class="card__left__subtitle">WEEK 1</p>
+                    <h2 class="card__left__title">Lorem impsum</h2>
+                    <hr class="card__left__trait">
+                    <p class="card__left__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolores consectetur distinctio blanditiis a asperiores. Voluptatem distinctio, suscipit molestias, dignissimos accusamus facere, debitis consequuntur cumque deserunt qui nemo maiores unde?</p>
+                </div>
             </div>
-        </div>
-
-        <!-- pagination -->
-         <ul class="card__pagination">
-            <li><a href="#">&lt;</a></li>
-            <li><a href="#">1</a></li>
-            <li><a href="#">2</a></li>
-            <li><a href="#">3</a></li>
-            <li><a href="#">4</a></li>
-            <li><a href="#">5</a></li>
-            <li><a href="#">&gt;</a></li>
-         </ul>
+    
+            <!-- pagination -->
+             <ul class="card__pagination">
+                <li><a href="#">&lt;</a></li>
+                <li><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#">&gt;</a></li>
+             </ul>
+        </section>
     </main>
 
 </body>
